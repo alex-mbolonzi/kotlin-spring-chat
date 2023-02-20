24dfc77681bc6dc3502abc805bbe8c6f5351ad68
alex-mbolonzi <almbolonzi@gail.com>
2023-02-20 16:16:39 +0300

    Modified PersistentMessageService.kt class to fix compile error

--- src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
+++ src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
@@ -1,12 +1,10 @@
 package com.example.kotlin.chat.service
 
+import com.example.kotlin.chat.asDomainObject
 import com.example.kotlin.chat.mapToViewModel
-import com.example.kotlin.chat.repository.ContentType
-import com.example.kotlin.chat.repository.Message
 import com.example.kotlin.chat.repository.MessageRepository
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
-import java.net.URL
 
 @Service
 @Primary
@@ -16,14 +14,11 @@ class PersistentMessageService(val messageRepository: MessageRepository) : Messa
         messageRepository.findLatest()
             .mapToViewModel() // now we can use the mentioned extension on List<Message>
 
-    override fun after(lastMessageId: String): List<MessageVM> =
-        messageRepository.findLatest(lastMessageId)
+    override fun after(messageId: String): List<MessageVM> =
+        messageRepository.findLatest(messageId)
             .mapToViewModel()
 
     override fun post(message: MessageVM) {
-        messageRepository.save(
-            with(message) { Message(content, ContentType.PLAIN, sent,
-                user.name, user.avatarImageLink.toString()) }
-        )
+        messageRepository.save(message.asDomainObject())
     }
 }
