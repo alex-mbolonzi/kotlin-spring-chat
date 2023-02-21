61c283ebc207605da701fe566d1f73d78b29030f
alex-mbolonzi <almbolonzi@gail.com>
2023-02-21 15:18:35 +0300

    Code Refactor - Added R2DBC configurations - Modified test cases

--- src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
@@ -22,11 +22,13 @@ import java.net.URI
 import java.net.URL
 import java.time.Instant
 import java.time.temporal.ChronoUnit.MILLIS
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 @SpringBootTest(
 	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 	properties = [
-		"spring.datasource.url=jdbc:h2:mem:testdb"
+		"spring.r2dbc.url=r2dbc:h2:mem:///testdb;USER=sa;PASSWORD=password"
 	]
 )
 class ChatKotlinApplicationTests {
@@ -43,101 +45,110 @@ class ChatKotlinApplicationTests {
 
 	@BeforeEach
 	fun setUp() {
-		val secondBeforeNow = now.minusSeconds(1)
-		val twoSecondBeforeNow = now.minusSeconds(2)
-		val savedMessages = messageRepository.saveAll(
-			listOf(
-				Message(
-					"*testMessage*",
-					ContentType.PLAIN,
-					twoSecondBeforeNow,
-					"test",
-					"http://test.com"
-				),
-				Message(
-					"**testMessage2**",
-					ContentType.MARKDOWN,
-					secondBeforeNow,
-					"test1",
-					"http://test.com"
-				),
-				Message(
-					"`testMessage3`",
-					ContentType.MARKDOWN,
-					now,
-					"test2",
-					"http://test.com"
+		runBlocking {
+			val secondBeforeNow = now.minusSeconds(1)
+			val twoSecondBeforeNow = now.minusSeconds(2)
+			val savedMessages = messageRepository.saveAll(
+				listOf(
+					Message(
+						"*testMessage*",
+						ContentType.PLAIN,
+						twoSecondBeforeNow,
+						"test",
+						"http://test.com"
+					),
+					Message(
+						"**testMessage2**",
+						ContentType.MARKDOWN,
+						secondBeforeNow,
+						"test1",
+						"http://test.com"
+					),
+					Message(
+						"`testMessage3`",
+						ContentType.MARKDOWN,
+						now,
+						"test2",
+						"http://test.com"
+					)
 				)
 			)
-		)
-		lastMessageId = savedMessages.first().id ?: ""
+			lastMessageId = savedMessages.first().id ?: ""
+		}
 	}
-
 	@AfterEach
 	fun tearDown() {
-		messageRepository.deleteAll()
+		runBlocking {
+			messageRepository.deleteAll()
+		}
 	}
 
 	@ParameterizedTest
 	@ValueSource(booleans = [true, false])
 	fun `test that messages API returns latest messages`(withLastMessageId: Boolean) {
-		val messages: List<MessageVM>? = client.exchange(
-			RequestEntity<Any>(
-				HttpMethod.GET,
-				URI("/api/v1/messages?lastMessageId=${if (withLastMessageId) lastMessageId else ""}")
-			),
-			object : ParameterizedTypeReference<List<MessageVM>>() {}).body
+		runBlocking {
+			val messages: List<MessageVM>? = client.exchange(
+				RequestEntity<Any>(
+					HttpMethod.GET,
+					URI("/api/v1/messages?lastMessageId=${if (withLastMessageId) lastMessageId else ""}")
+				),
+				object : ParameterizedTypeReference<List<MessageVM>>() {}).body
 
-		if (!withLastMessageId) {
-			assertThat(messages?.map { with(it) { copy(id = null, sent = sent.truncatedTo(MILLIS)) } })
-				.first()
-				.isEqualTo(
+			if (!withLastMessageId) {
+				assertThat(messages?.map { with(it) { copy(id = null, sent = sent.truncatedTo(MILLIS)) } })
+					.first()
+					.isEqualTo(
+						MessageVM(
+							"*testMessage*",
+							UserVM("test", URL("http://test.com")),
+							now.minusSeconds(2).truncatedTo(MILLIS)
+						)
+					)
+			}
+
+			assertThat(messages?.map { it.prepareForTesting() })
+				.containsSubsequence(
 					MessageVM(
-						"*testMessage*",
-						UserVM("test", URL("http://test.com")),
-						now.minusSeconds(2).truncatedTo(MILLIS)
+						"<body><p><strong>testMessage2</strong></p></body>",
+						UserVM("test1", URL("http://test.com")),
+						now.minusSeconds(1).truncatedTo(MILLIS)
+					),
+					MessageVM(
+						"<body><p><code>testMessage3</code></p></body>",
+						UserVM("test2", URL("http://test.com")),
+						now.truncatedTo(MILLIS)
 					)
 				)
 		}
-
-		assertThat(messages?.map { it.prepareForTesting() })
-			.containsSubsequence(
-				MessageVM(
-					"<body><p><strong>testMessage2</strong></p></body>",
-					UserVM("test1", URL("http://test.com")),
-					now.minusSeconds(1).truncatedTo(MILLIS)
-				),
-				MessageVM(
-					"<body><p><code>testMessage3</code></p></body>",
-					UserVM("test2", URL("http://test.com")),
-					now.truncatedTo(MILLIS)
-				)
-			)
 	}
 
 
 	@Test
 	fun `test that messages posted to the API is stored`() {
-		client.postForEntity<Any>(
-			URI("/api/v1/messages"),
-			MessageVM(
-				"`HelloWorld`",
-				UserVM("test", URL("http://test.com")),
-				now.plusSeconds(1)
+		runBlocking {
+			client.postForEntity<Any>(
+				URI("/api/v1/messages"),
+				MessageVM(
+					"`HelloWorld`",
+					UserVM("test", URL("http://test.com")),
+					now.plusSeconds(1)
+				)
 			)
-		)
 
-		messageRepository.findAll()
-			.first { it.content.contains("HelloWorld") }
-			.apply {
-				assertThat(this.prepareForTesting())
-					.isEqualTo(Message(
-						"`HelloWorld`",
-						ContentType.MARKDOWN,
-						now.plusSeconds(1).truncatedTo(MILLIS),
-						"test",
-						"http://test.com"
-					))
-			}
+			messageRepository.findAll()
+				.first { it.content.contains("HelloWorld") }
+				.apply {
+					assertThat(this.prepareForTesting())
+						.isEqualTo(
+							Message(
+								"`HelloWorld`",
+								ContentType.MARKDOWN,
+								now.plusSeconds(1).truncatedTo(MILLIS),
+								"test",
+								"http://test.com"
+							)
+						)
+				}
+		}
 	}
 }
